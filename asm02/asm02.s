fedf3dac041d5b7351c0f981c4798853b0bd31cc
root <root@UTILISA-BBDDJ8E.localdomain>
2025-09-30 14:10:53 +0200

    Correction asm02: vérification stricte de l'entrée 42\n

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -9,33 +9,37 @@ section .text
     global _start
 
 _start:
-    ; lire sur stdin
-    mov rax, 0           ; syscall read
-    mov rdi, 0           ; fd = stdin
-    mov rsi, input       ; buffer
-    mov rdx, 3           ; max 3 octets
+    ; lire stdin
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input
+    mov rdx, 3
     syscall
 
-    ; vérifier "42"
+    ; vérifier chaque caractère
     mov al, [input]
     cmp al, '4'
-    jne wrong
+    jne fail
     mov al, [input+1]
     cmp al, '2'
-    jne wrong
+    jne fail
+    mov al, [input+2]
+    cmp al, 10          ; saut de ligne attendu
+    jne fail
 
-    ; si "42", afficher
+    ; afficher "1337"
     mov rax, 1
     mov rdi, 1
     mov rsi, outmsg
     mov rdx, outlen
     syscall
 
+    ; exit 0
     mov rax, 60
-    xor rdi, rdi         ; exit 0
+    xor rdi, rdi
     syscall
 
-wrong:
+fail:
     mov rax, 60
-    mov rdi, 1           ; exit 1
+    mov rdi, 1          ; exit 1
     syscall
