22c19249bdbe036f764853d624b6fdc0f61b7f36
root <root@UTILISA-BBDDJ8E.localdomain>
2025-10-01 12:26:45 +0200

    Correction asm05: exit 1 si pas d'argument

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -2,17 +2,24 @@ section .text
     global _start
 
 _start:
-    mov rax, [rsp]          ; argc
-    cmp qword [rsp], 2      ; au moins 1 arg ?
-    jb exit_ok
+    mov rax, [rsp]
+    cmp qword [rsp], 2      ; argc >= 2 ?
+    jb no_param             ; si pas de param, exit 1
 
     mov rsi, [rsp+16]       ; argv[1]
-    mov rdi, 1              ; stdout
+    mov rdi, 1              ; STDOUT
     call print_str
+
+    jmp exit_ok
+
+no_param:
+    mov rax, 60
+    mov rdi, 1              ; exit code 1 si pas de param
+    syscall
 
 exit_ok:
     mov rax, 60
-    xor rdi, rdi
+    xor rdi, rdi            ; exit code 0
     syscall
 
 print_str:
@@ -26,6 +33,5 @@ print_str:
 .write:
     mov rax, 1
     mov rdi, 1
-    ; respecte que rsi ne soit pas modifié
     syscall
     ret
